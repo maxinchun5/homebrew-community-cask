f8c182605442db5ed996d2fe2dcbdbba173c647a
Jun Ma <maxinchun5@icloud.com>
2026-09-21 21:06:24 +0800

    chromium-gost 152.0.7977.134

--- Casks/c/chromium-gost.rb
+++ Casks/c/chromium-gost.rb
@@ -1,19 +1,28 @@
 cask "chromium-gost" do
   arch arm: "arm64", intel: "amd64"
 
-  version "150.0.7871.224"
-  sha256 arm:   "9bc80d7ecd9caf43459d89d4c4880f95aae333b4a73ab6e886b59018cae1e2e7",
-         intel: "e4dbe5b029c0c688c5c077e5818a9e2ad00caad76feb697f6c8f9d3b016f709a"
+  on_monterey :or_older do
+    version "150.0.7871.224"
+    sha256 arm:   "a0da4b01daea8f45bdedf6ec085cc03205113c9f635a69b74f7fd8d62aaa9a79",
+           intel: "c992a36f1059bec4305db3880ebbdf1b4327c1477f3e2389f3662418133fa2ad"
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_ventura :or_newer do
+    version "152.0.7977.134"
+    sha256 arm:   "a0da4b01daea8f45bdedf6ec085cc03205113c9f635a69b74f7fd8d62aaa9a79",
+           intel: "c992a36f1059bec4305db3880ebbdf1b4327c1477f3e2389f3662418133fa2ad"
+    livecheck do
+      url :url
+      strategy :github_latest
+    end
+  end
 
   url "https://github.com/deemru/Chromium-Gost/releases/download/#{version}/chromium-gost-#{version}-macos-#{arch}.tar.bz2"
   name "Chromium-Gost"
   desc "Browser based on Chromium with support for GOST cryptographic algorithms"
   homepage "https://github.com/deemru/Chromium-Gost"
-
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
 
   depends_on macos: :monterey
 
