72d968e852cd13b7fd469b4be2e77db1b54adbee
Jun Ma <maxinchun5@icloud.com>
2026-09-21 21:17:09 +0800

    angry-ip-scanner 3.10.0

--- Casks/a/angry-ip-scanner.rb
+++ Casks/a/angry-ip-scanner.rb
@@ -1,11 +1,11 @@
 cask "angry-ip-scanner" do
   arch arm: "Arm64", intel: "X86"
 
-  version "3.9.3"
-  sha256 arm:   "e8997d2ff12f7322ecb3dd0b4c4a410187aa8a26ad89772c833af6de781dfd29",
-         intel: "554c5a14ee32c1ef1812f5382209afee75548c350ed767cf924bc5e0532f403b"
+  version "3.10.0"
+  sha256 arm:   "4047730a0dd8f8617bdc876d72b55b54b16ad4243c05793cb85bed9c945ec885",
+         intel: "0917fdca45ac90d786f3b927275f9a797c089580e3b12218ebd53edfe9e061ae"
 
-  url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-mac#{arch}-#{version}.zip"
+  url "https://github.com/angryip/ipscan/releases/download/#{version}/ipscan-#{version}-mac#{arch}.zip"
   name "Angry IP Scanner"
   desc "Network scanner"
   homepage "https://angryip.org/"
