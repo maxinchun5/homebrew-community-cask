234d6d6308ca3a3854622015e0b8c72beaaf0a68
Jun Ma <maxinchun5@icloud.com>
2026-09-21 21:43:36 +0800

    aria2d 1.4.2,484

--- Casks/a/aria2d.rb
+++ Casks/a/aria2d.rb
@@ -1,18 +1,30 @@
 cask "aria2d" do
-  version "1.4.1,470"
-  sha256 "71aea297a3e47294a1097506945ba943915bda2784f93eead53487b5ccea547c"
+  on_big_sur :or_older do
+    version "1.4.1,470"
+    sha256 "71aea297a3e47294a1097506945ba943915bda2784f93eead53487b5ccea547c"
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_monterey :or_newer do
+    version "1.4.2,484"
+    sha256 "5ca7e037d934eac8d6b545db375e23fec1cbfacd99705f0b8d08eb0f98643e17"
+    livecheck do
+      url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Appcast.xml"
+      strategy :sparkle do |items|
+        items.map(&:nice_version)
+      end
+    end
+  end
 
-  url "https://github.com/xjbeta/Aria2D/releases/download/#{version.csv.first}%28#{version.csv.second}%29/Aria2D.zip"
+  if Gem::Version.new(version.csv.first) >= Gem::Version.new("1.4.2")
+    url "https://github.com/xjbeta/Aria2D/releases/download/#{version.csv.first}/Aria2D.zip"
+  else
+    url "https://github.com/xjbeta/Aria2D/releases/download/#{version.csv.first}%28#{version.csv.second}%29/Aria2D.zip"
+  end
   name "Aria2D"
   desc "Aria2 GUI"
   homepage "https://github.com/xjbeta/Aria2D"
-
-  livecheck do
-    url "https://raw.githubusercontent.com/xjbeta/AppUpdaterAppcasts/master/Aria2D/Appcast.xml"
-    strategy :sparkle do |items|
-      items.map(&:nice_version)
-    end
-  end
 
   depends_on :macos
 
