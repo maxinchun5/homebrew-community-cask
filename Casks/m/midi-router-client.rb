c49ed66af892257b506afd269a01e5855c9259a5
Jun Ma <maxinchun5@icloud.com>
2026-09-23 11:44:24 +0800

    cask(midi-router-client): versioning and livecheck
    Refactor versioning for midi-router-client based on macOS version. Update livecheck configuration.

--- Casks/m/midi-router-client.rb
+++ Casks/m/midi-router-client.rb
@@ -1,18 +1,28 @@
 cask "midi-router-client" do
-  version "2.45.0"
-  sha256 "eab6052931f7157ffd29e28781e87cc70fa3e51174757ae7f73d6d35efae808a"
+  on_tahoe :or_older do
+    version "2.44.0"
+    sha256 "7674176f095cc734d45d305ee7da9ee5a83314c9709114f3d590c62fd7e36f14"
+
+    livecheck do
+      skip "Legacy version"
+    end
+  end
+  on_golden_gate :or_newer do
+    version "2.45.0"
+    sha256 "eab6052931f7157ffd29e28781e87cc70fa3e51174757ae7f73d6d35efae808a"
+
+    livecheck do
+      url :url
+      regex(%r{url=.*?/midi-router-client[._-]v?(\d+(?:\.\d+)+)[._-]Darwin\.(?:dmg|zip)}i)
+    end
+  end
 
   url "https://downloads.sourceforge.net/midi-router-client/midi-router-client-#{version}-Darwin.dmg"
   name "Midi Router Client"
   desc "Create routes from anywhere to anywhere"
   homepage "https://sourceforge.net/projects/midi-router-client/"
 
-  livecheck do
-    url :url
-    regex(%r{url=.*?/midi-router-client[._-]v?(\d+(?:\.\d+)+)[._-]Darwin\.(?:dmg|zip)}i)
-  end
-
-  depends_on macos: :golden_gate
+  depends_on macos: :tahoe
 
   app "midi-router-client.app"
 
