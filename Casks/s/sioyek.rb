5a9268b4c513a32fd9d5fe1dbe5457990c82089c
Jun Ma <maxinchun5@icloud.com>
2026-09-25 20:11:29 +0800

    cask(sioyek): fix command-wrapper xattr permission error

--- Casks/s/sioyek.rb
+++ Casks/s/sioyek.rb
@@ -20,6 +20,7 @@ cask "sioyek" do
                   executable: "#{appdir}/sioyek.app/Contents/MacOS/sioyek"
 
   postflight_steps do
+    run "/bin/chmod", args: ["-R", "+w", "{{staged_path}}/.homebrew-command-wrappers"]
     run "/usr/bin/xattr", args: ["-r", "-d", "com.apple.quarantine", "{{staged_path}}"]
   end
 
