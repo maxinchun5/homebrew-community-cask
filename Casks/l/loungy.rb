7b3d1ac0555b1c6fd1bedfd5b842b388f779da49
Jun Ma <maxinchun5@icloud.com>
2026-09-25 19:35:55 +0800

    cask(loungy): fix `deprecate!` and `livecheck` stanza order

--- Casks/l/loungy.rb
+++ Casks/l/loungy.rb
@@ -7,12 +7,12 @@ cask "loungy" do
   desc "Application launcher"
   homepage "https://github.com/MatthiasGrandl/Loungy"
 
-  deprecate! date: "2026-08-25", because: :discontinued
-
   livecheck do
     url :url
     strategy :github_latest
   end
+
+  deprecate! date: "2026-08-25", because: :discontinued
 
   depends_on :macos
 
